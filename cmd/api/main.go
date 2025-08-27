6a23423c95c46d1d374cb182b923ea77ab3c5b63
Dian Rahmaji <dianrahmaji@gmail.com>
2025-08-27 20:15:50 +0700

    fix: command line flags
    - `maxOpenConns`
    - `maxIdleConns`
    _ `maxIdleTime`

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -65,9 +65,9 @@ func main() {
 
 	flag.StringVar(&cfg.db.dsn, "db-dsn", dsn, "PostgreSQL DSN")
 
-	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-cons", 25, "PostgreSQL max open connections")
-
-	flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time", 25, "PostgreSQL max connection idle time")
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "PostgreSQL max connection idle time")
 
 	db, err := openDB(cfg)
 	if err != nil {
