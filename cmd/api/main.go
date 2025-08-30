fc112c6cf4362d34515b109a5887029b354df3fc
Dian Rahmaji <dianrahmaji@gmail.com>
2025-08-30 23:14:43 +0700

    feat: add custom metrics
    - `version`
    - `goroutines`
    - `database`
    - `timestamp`

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"context"
 	"database/sql"
+	"expvar"
 	"flag"
 	"fmt"
 	"log/slog"
 	"os"
+	"runtime"
 	"strconv"
 	"strings"
 	"sync"
@@ -126,6 +128,20 @@ func main() {
 	defer db.Close()
 
 	logger.Info("database connection pool established")
+
+	expvar.NewString("version").Set(version)
+
+	expvar.Publish("goroutines", expvar.Func(func() any {
+		return runtime.NumGoroutine()
+	}))
+
+	expvar.Publish("database", expvar.Func(func() any {
+		return db.Stats()
+	}))
+
+	expvar.Publish("timestamp", expvar.Func(func() any {
+		return time.Now().Unix()
+	}))
 
 	app := &application{
 		config: cfg,
