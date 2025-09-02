f6381e74290c5e8eb18cabde5cf39d2a0f44a612
Dian Rahmaji <dianrahmaji@gmail.com>
2025-09-02 20:52:21 +0700

    feat: configure `@tanstack/react-query`

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,11 +1,18 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 const RootLayout = () => (
 	<>
 		<Outlet />
 		<TanStackRouterDevtools initialIsOpen={false} />
+		<ReactQueryDevtools initialIsOpen={false} />
 	</>
 );
 
-export const Route = createRootRoute({ component: RootLayout });
+export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
+	{
+		component: RootLayout,
+	},
+);
